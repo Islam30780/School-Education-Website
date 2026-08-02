2527dc2deda62e0c1175cff1cee717f101b20a75
islamabusaleem <i.n.abusaleem@students.ptuk.edu.ps>
2026-08-03 02:04:30 +0300

    used swiper in index page

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,19 +1,33 @@
 import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-export function Sliders(){
-    const animatedSlider = document.querySelector(".testimonials-swiper")
-    if(!animatedSlider){return;}
-   new Swiper(".testimonials-swiper", {
+export function Sliders() {
+  const animatedSlider = document.querySelector(".testimonials-swiper");
+
+  if (!animatedSlider) return;
+
+  new Swiper(".testimonials-swiper", {
+    modules: [Navigation, Pagination],
     loop: true,
     spaceBetween: 24,
     slidesPerView: 1,
+    pagination: {
+      el: ".testimonials-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".testimonials-next",
+      prevEl: ".testimonials-prev",
+    },
     breakpoints: {
       768: {
-        slidesPerView: 2,
+        slidesPerView: 1,
       },
       1024: {
-        slidesPerView: 3,
+        slidesPerView: 2,
       },
     },
   });
